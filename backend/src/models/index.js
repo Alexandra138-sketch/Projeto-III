4131c4de4cae7b348b9f97dafe4814fbfb4548e1
Alexandra138-sketch <estgv19355@alunos.estgv.ipv.pt>
2026-06-30 16:46:25 +0100

    Corrigir para render funcionar

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -8,8 +8,6 @@ const Noticia        = require('./Noticia');
 const Contacto       = require('./Contacto');
 const ConteudoPagina = require('./ConteudoPagina');
 const LogAtividade   = require('./LogAtividade');
-const Ativo          = require('./Ativo');
-const Pedido         = require('./Pedido');
 
 /* ── Associações ── */
 
@@ -32,16 +30,7 @@ Noticia.belongsTo(Utilizador, { as: 'autor', foreignKey: 'criado_por' });
 // Log pertence a um utilizador
 LogAtividade.belongsTo(Utilizador, { as: 'utilizador', foreignKey: 'utilizador_id' });
 
-// Ativo pertence a um cliente
-Ativo.belongsTo(Cliente, { as: 'cliente', foreignKey: 'cliente_id' });
-Cliente.hasMany(Ativo,   { as: 'ativos',  foreignKey: 'cliente_id' });
-
-// Pedido pertence a um cliente e pode ter uma resposta de um utilizador (gestor)
-Pedido.belongsTo(Cliente,    { as: 'cliente',    foreignKey: 'cliente_id' });
-Pedido.belongsTo(Utilizador, { as: 'respondedor', foreignKey: 'respondido_por' });
-Cliente.hasMany(Pedido,   { as: 'pedidos', foreignKey: 'cliente_id' });
-
 module.exports = {
   Utilizador, Cliente, Incidente, Documento, Servico, Mensagem,
-  Noticia, Contacto, ConteudoPagina, LogAtividade, Ativo, Pedido,
+  Noticia, Contacto, ConteudoPagina, LogAtividade,
 };
